2cf5af0c85e6bcc5f341b00bd6fce5b2f470231f
Panchis7u7 <smadrigal.rod@gmail.com>
2020-03-30 18:53:49 -0600

    Ventanas

---
+++ PRII_U207_VentanasMDI/src/prii_u207_ventanasmdi/Pograma.java
@@ -0,0 +1,15 @@
+package prii_u207_ventanasmdi;
+/**
+ * @author Katherine Arzate Serrano
+ * @author Carlos Sebastian Madrigal Rodriguez
+ */
+public class Pograma {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaPadre v = new VentanaPadre();
+        v.setTitle("Ordenes de Trabajo");
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
